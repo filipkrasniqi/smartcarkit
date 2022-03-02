9e2067769bb61622c756decaf806ea57aadf6046
Filip Krasniqi <filip.krasniqi@polimi.it>
2022-03-02 10:37:47 +0100

    Moving car

---
+++ smartcarkit/main.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append("../Code/Server/")
+
+from Motor import *
+
+if __name__ == "__main__":
+
+    PWM = Motor()
+    # D: rotate clockwise
+    # A: rotate counterclockwise
+    # W: proceed forward
+    # S: proceed backward
+    # X: stop current operation
+    # V: stop everytihing
+
+    in_val = ""
+    while in_val != "V":
+        # while in_val != "X":
+        in_val = input("Gimme input (WASD + X + V)").upper()
+        if in_val == "W":
+            PWM.setMotorModel(1000, 1000, 1000, 1000)  # Forward
+        elif in_val == "S":
+            PWM.setMotorModel(-1000, -1000, -1000, -1000)  # Backward
+        elif in_val == "A":
+            PWM.setMotorModel(-1500, -1500, 2000, 2000)  # Left
+        elif in_val == "D":
+            PWM.setMotorModel(2000, 2000, -1500, -1500)  # Right
+        else:
+            PWM.setMotorModel(0, 0, 0, 0)  # Stop
+
+    print("End of program")
